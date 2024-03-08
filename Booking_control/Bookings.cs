374272ff19420852a9e25dee4dd692a2503d98a4
EliaBuratti <elia.buratti.dev@gmail.com>
2024-03-08 18:24:36 +0100

    Aggiungere i file di progetto.

---
+++ Booking_control/Bookings.cs
@@ -0,0 +1,110 @@
+﻿namespace Booking_control
+{
+    internal class Bookings
+    {
+        private int SeatNumber;
+        private int GroupMax;
+        private int SeatAvaiable;
+        private string[] SeatOccuped;
+
+
+        public Bookings(int seatNumber, int groupMax)
+        {
+            SeatNumber = seatNumber;
+            GroupMax = groupMax;
+            SeatAvaiable = seatNumber;
+            SeatOccuped = new string[seatNumber];
+        }
+
+        public string NewBooking(string NameBooking, int NumSeat)
+        {
+            if (CheckSeats(NumSeat).avaiable)
+            {
+                string seatBooked = "";
+                for (int i = 0, j = NumSeat; i < SeatNumber; i++)
+                {
+                    if (SeatOccuped[i] == null && j > 0)
+                    {
+                        SeatOccuped[i] = NameBooking;
+                        seatBooked += i + " ";
+                        SeatAvaiable--;
+                        j--;
+                    }
+                }
+                SeatNumber -= NumSeat;
+                return "Congrats! " + NameBooking + ". You're seat are booked!\nSeat number:" + seatBooked;
+            }
+
+            return CheckSeats(NumSeat).message;
+        }
+
+        public (int Seat, bool avaiable, string message) CheckSeats(int NumSeat)
+        {
+            string message = "Sorry, the seat: " + NumSeat + ". It's not avaiable." + "You must booking for maximum " + GroupMax;
+
+            //Console.WriteLine(SeatAvaiable);
+            if (NumSeat <= GroupMax && NumSeat <= SeatAvaiable)
+            {
+                SeatAvaiable = 0;
+
+                foreach (var Seat in SeatOccuped)
+                {
+                    if (Seat == null)
+                    {
+                        Console.WriteLine(SeatAvaiable);
+                        SeatAvaiable++;
+                    }
+                }
+
+
+                message = "Your seat: " + NumSeat + " are avaiable!";
+
+                if (NumSeat < SeatAvaiable )
+                {
+                    message = "Sorry we don't have much seat for your booking, we have avaiable: " + SeatAvaiable;
+                }
+
+                return (SeatAvaiable, SeatAvaiable >= NumSeat, message);
+            }
+
+            message = (SeatAvaiable == 0 ? "Sorry, the cinema is Full." : message);
+
+            return (SeatAvaiable, false, message);
+
+
+        }
+
+        public string DeleteBooking(string Name)
+        {
+
+            if (!SeatOccuped.Contains(Name))
+            {
+                return "Nothing to delete, this name: \"" + Name + "\" isn't matched with any booking.";
+            }
+
+            for (int i = 0; i <= SeatOccuped.Length - 1; i++)
+            {
+                if (SeatOccuped[i] == Name)
+                {
+                    SeatOccuped.SetValue(null, i);
+                    SeatNumber++;
+                    SeatAvaiable++;
+                } 
+            }
+
+            return "Your Booking: " + Name + ", was deleted!";
+        }  
+
+        public string SeeBooking()
+        {
+            string mapOccupation = "";
+
+            for (int i = 0; i < SeatOccuped.Length; i++)
+            {
+                mapOccupation += "\nSeat: " + i + "   name: " + (SeatOccuped[i] == null ? "Empty" : SeatOccuped[i]);
+            }
+            return mapOccupation;
+        }
+        
+    }
+}
---
+++ Booking_control/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Booking_control
+{
+    internal class Cinema
+    {
+        static void Main(string[] args)
+        {
+            int SeatCinema = 50;
+            int GroupMaxBooking = 50;
+            Bookings newBooking = new Bookings(SeatCinema, GroupMaxBooking);
+            int num = 0;
+
+            while (num != 5)
+            {
+                Console.WriteLine("Welcome in Experis Cinema!, What do you want to do?");
+
+                Console.WriteLine("1 new Booking \n2 check seat are avaiable \n3 delete booking \n4 wiew booking \n5 exit");
+                try
+                {
+                    num = Convert.ToInt32(Console.ReadLine());
+                } 
+                catch (Exception ex) 
+                {
+                    Console.Write($"{ex.Message}\n");
+                }
+                
+
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("What is your name?");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("How many seat do you want to booking?");
+                        int SeatNum = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(newBooking.NewBooking(name, SeatNum));
+                        break;
+
+                    case 2:
+                        Console.WriteLine("How many seat do you want to booking?");
+                        SeatNum = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(newBooking.CheckSeats(SeatNum).message);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("What is your name?");
+                        name = Console.ReadLine();
+
+                        Console.WriteLine(newBooking.DeleteBooking(name));
+                        break;
+
+                    case 4:
+                        Console.WriteLine(newBooking.SeeBooking());
+                        break;
+                }
+
+            }
+
+            Console.WriteLine("exit");
+        }
+    }
+}
